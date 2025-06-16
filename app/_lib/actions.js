2d2cf82f8f78ed96b841243ad71f28a606da3c78
Bolaji87 <sakabolaji77@gmail.com>
2025-06-16 17:58:45 +0100

    delete/remove reservation immediately using useOptimistic hook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -27,6 +27,8 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 export async function deleteReservation(bookingId) {
+  // await new Promise((res) => setTimeout(res, 2000));
+  // throw new Error();
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
   const guestBookings = await getBookings(session.user.guestId);
